797659cd12477cd935eeb903611062df66acd692
Mostafa Ramezanpour <mr1528@yahoo.com>
2019-06-04 18:59:24 +0430

    Create first.py

---
+++ first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+import requests
+from bs4 import BeautifulSoup
+r = requests.get('http://www.itico.ir')
+#r.text
+soup = BeautifulSoup(r.text, "html.parser")
+#print( soup.prettify())
+#print(soup.find(text="TourBarg"))
+print(soup.br)
+
+
+# In[ ]:
+
+
+
+
