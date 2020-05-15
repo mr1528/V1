6ccf9716db04d085fa471ef39525ad2745e83d3c
mr1528 <mr1528@yahoo.com>
2020-05-15 23:46:31 +0430

    3rd

--- first.py
+++ first.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 r = requests.get('http://www.itico.ir')
 #r.text
 soup = BeautifulSoup(r.text, "html.parser")
-#print( soup.prettify())
+print( soup.prettify())
 #print(soup.find(text="TourBarg"))
 print(soup)
 
